baee58966760f610728b6a2e2c8893518483f9a8
Dharshini151 <108514100+Dharshini151@users.noreply.github.com>
2023-08-22 00:31:37 +0530

    Add files via upload

---
+++ src/test/java/com/dharshini/Restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dharshini.Restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
